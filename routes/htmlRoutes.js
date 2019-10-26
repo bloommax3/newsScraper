dce603463c464afab22972b0db34b7f2b1f2060b
MAX_BLOOM <bloommax3@gmail.com>
2019-10-25 22:38:00 -0400

    routes working

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -11,5 +11,12 @@ module.exports = function(app){
         })
     })
 
-    
+    app.get("/archived", function(req, res){
+        db.Article.find({})
+        .then(function(data){
+            res.render("archived", {data: data})
+        }).catch(function(err){
+            console.log(err)
+        })
+    })
 }
