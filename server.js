171c430d93f28de2ff039a036e98984c7036605d
MAX_BLOOM <bloommax3@gmail.com>
2019-10-26 22:34:19 -0400

    my port wasn't working

--- server.js
+++ server.js
@@ -12,7 +12,9 @@ var cheerio = require("cheerio");
 // Require all models
 var db = require("./models");
 
-var PORT = 3000;
+// Set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var PORT = process.env.PORT || 3000;
 
 // Initialize Express
 var app = express();
